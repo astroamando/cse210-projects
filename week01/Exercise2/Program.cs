25368760ed56c6d94e3cf274505b457dde6f43e7
astroamando <amando.filho83@gmail.com>
2025-09-15 20:41:13 -0600

    basic requirements

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("What is your grade percentage?. ");
+
+        string userinput = Console.ReadLine();
+
+        int grade = int.Parse(userinput);
+
+        if (grade >= 90)
+        {
+            Console.WriteLine("You got an A.");
+        }
+        else if (grade >= 80 & grade < 90)
+        {
+            Console.WriteLine("You got a B.");
+        }
+        else if (grade >= 70 & grade < 80)
+        {
+            Console.WriteLine("You got a C.");
+        }
+        else if (grade >= 60 & grade < 70)
+        {
+            Console.WriteLine("You got a D.");
+        }
+        else if (grade < 60)
+        {
+            Console.WriteLine("You got an F");
+        }
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed.");
+        }
     }
 }
